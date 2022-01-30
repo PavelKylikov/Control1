9a5ac47f030c95b3f88fa1da8b9f97832508df80
PavelKylikov <pawel-kkk@yandex.ru>
2022-01-30 09:23:23 +0300

    Определили колличество элементов в массиве длинной 3 и менее символов

--- Program.cs
+++ Program.cs
@@ -13,3 +13,16 @@ Console.WriteLine();
 // Создаём массив заполнив его текстами
 string[] Array1 = {"hello", "Ура", "world", "по", ";%:?*", "лу", "f4ы3", "чил", "sgbcr", "ось", "!"};
 PrintArray(Array1);
+Console.WriteLine();
+// Определяем колличество элементов в массиве длинной 3 и менее символов
+int long3 = 0;
+for (int i = 0; i < Array1.Length; i++)
+{
+    string Text = Array1[i];
+    int length = Text.Length;
+    if (length <= 3)
+    {
+        long3 = long3 + 1;
+    }
+}
+Console.WriteLine(long3);
