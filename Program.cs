e94923d87e036c3c69c6120f0ae64dbbcc80e998
PavelKylikov <pawel-kkk@yandex.ru>
2022-01-30 08:38:39 +0300

    Создал метод вывода массива в терминал

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Создал метод вывода массива (PrintArray) в консоль 
+void PrintArray(string[] Array)
+{
+    int count = Array.Length;
+    int position = 0;
+    while (position < count)
+    {
+         Console.Write(Array[position] + " ");
+         position++;
+    }
+}
+Console.WriteLine();
